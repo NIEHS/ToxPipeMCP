062132695e1ff667225626884fdac966171244d9
Amlan Talukder <amlanaccount@gmail.com>
2026-01-31 18:31:35 -0500

    Added MCP client

--- src/rag/__init__.py
+++ src/rag/__init__.py
@@ -1,6 +1,3 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from .llms import getAIModel
 from .utils import State, Config
 from langchain_classic.llms import BaseLLM
